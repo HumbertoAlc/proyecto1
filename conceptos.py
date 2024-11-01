2905f8957e05d08acb245e0c7eed97b09db18993
humberto <humalcgar@gmail.com>
2024-11-01 13:38:36 -0600

    agrego entorno virtual

---
+++ conceptos.py
@@ -0,0 +1,12 @@
+## github : es la nuve o red social de todos los programadores
+## un branch : sirve para  tener el control de cada una de las versiones de software
+## un commit : son los cambios que realizamos dentro de los branches
+## versiones 1.0.0 : el 1 se refiere a cambios mayores en el sofware, el 0 son cambios medianos que no perjudican al funcionamiento
+## del software y el ultimo cero son cambios muy puntuales en el software 
+
+## requirements
+## son lo que nosotros le enviamos al hosting que va desplegr el proyecto (librerias)
+
+## entorno virtual
+# es un espacio completa,ente vacio para trabajar en un proyecto de forma que instalemos dentro de este espacio todas las librerias
+## que vamos a usar en el mismo
